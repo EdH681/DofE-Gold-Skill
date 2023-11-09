33ae8df4f65a49782423ae22bc52a30245615c7c
Edward <edwardhornby07@gmail.com>
2023-11-09 21:12:35 +0000

    Added character button placeholder and stats viewer button placeholder

--- menu.py
+++ menu.py
@@ -3,8 +3,6 @@ __name__ = 'menu'
 
 '''
 To do:
-- character button
-- stats button
 - settings button 
 - complete assets and update buttons
 '''
@@ -14,10 +12,26 @@ def play_button(surface, colour, size, screensize):
     width, height = size, size
     x =(screensize[0]/2) - (width/2)
     y =(screensize[1]/2) - (height/2)
-    pygame.draw.rect(surface, colour, (x, y, width, height), 1)
+    play = pygame.draw.rect(surface, colour, (x, y, width, height), 1)
+
+
+def character_change(surface, colour, size, screensize):
+    width, height = size, size
+    x = (screensize[0]/2) - (width/2) - (size * 1.5)
+    y = (screensize[1] / 2) - (size/2)
+    character = pygame.draw.rect(surface, colour, (x, y, width, height), 1)
+
+
+def stats_viewer(surface, colour, size, screensize):
+    width, height = size, size
+    x = (screensize[0]/2) - (width/2) + (size * 1.5)
+    y = (screensize[1] / 2) - (size/2)
+    stats = pygame.draw.rect(surface, colour, (x, y, width, height), 1)
 
 
 def menu(surface, screensize=(1920, 1080)):
     surface.fill('black')
     play_button(surface, 'red', 300, screensize)
+    character_change(surface, 'red', 200, screensize)
+    stats_viewer(surface, 'red', 200, screensize)
     pygame.display.update()
